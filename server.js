7c2dbbd643c91e084f0d9a6a1347421dc32a662b
Mariea Johnson <marieaajohnson@gmail.com>
2018-06-03 16:43:38 -0400

    created initial files for app

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+
+// Set up Express Router
+var router = express.Router();
+
+require("./config/routes")(router);
+
+
+// Make public folder static directory
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoNewsarticles";
+
+mongoose.connect(db, function(error){
+  if (error) {
+    console.log(error);
+  }
+  else{
+    console.log("mongoose connection is successful");
+  }
+});
+
+app.listen(PORT, function(){
+  console.log("Listening on port:" + PORT);
+});
